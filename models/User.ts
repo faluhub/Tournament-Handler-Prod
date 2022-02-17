23e751317579905e7200c8708419c31ccacd5a25
BlueSmoke <blu3smok3mc@gmail.com>
2022-02-17 18:59:16 -0500

    Initial mongoose schemas

---
+++ models/User.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    active: Boolean,
+    country: String,
+    participatingTournaments: [mongoose.Schema.Types.ObjectId],
+    spectatingTournaments: [mongoose.Schema.Types.ObjectId]
+});
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
